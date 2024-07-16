3709ad202e44c7f98ad28a2c3e69dbfafae08330
Nokashi <44207343+Nokashi@users.noreply.github.com>
2024-07-16 20:23:37 +0300

    Add Profile Page

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -3,11 +3,11 @@ const User = require('../models/user')
 const asyncHandler = require('express-async-handler');
 
 const quizzes = [
-    { id: 1, title: 'Quiz for Course 1: Introduction to Programming', description: 'Test your knowledge on the basics of programming.' },
-    { id: 2, title: 'Quiz for Course 2: Intermediate Programming', description: 'Assess your skills in intermediate programming topics.' },
-    { id: 3, title: 'Quiz for Course 3: Advanced Programming', description: 'Challenge yourself with advanced programming questions.' },
-    { id: 4, title: 'Quiz for Course 4: Expert Programming', description: 'Prove your expertise with these difficult programming questions.' },
-    { id: 'All', title: 'Quiz on Everything you have learned', description: 'Prove your expertise on all taught materials.' }
+    { id: 1, title: 'Quiz for Course 1', description: 'Test your knowledge on Course 1' },
+    { id: 2, title: 'Quiz for Course 2', description: 'Test your knowledge on Course 2' },
+    { id: 3, title: 'Quiz for Course 3', description: 'Test your knowledge on Course 3' },
+    { id: 4, title: 'Quiz for Course 4', description: 'Test your knowledge on Course 4'},
+    { id: 5, title: 'Quiz on Everything you have learned', description: 'Test your knowledge on all Courses'}
 ];
 
 
--- routes/quizzes.js
+++ routes/quizzes.js
@@ -8,16 +8,16 @@ router.get('/', quizController.quiz_list);
 
 // %% QUIZ PAGES %%
 
-router.get('/quiz1', quizController.quiz1);
+router.get('/1', quizController.quiz1);
 
-router.get('/quiz2', quizController.quiz2);
+router.get('/2', quizController.quiz2);
 
-router.get('/quiz3', quizController.quiz3);
+router.get('/3', quizController.quiz3);
 
-router.get('/quiz4', quizController.quiz4);
+router.get('/4', quizController.quiz4);
 
-router.get('/quizAll', quizController.quizAll);
+router.get('/5', quizController.quizAll);
 
-router.post('/quiz1', quizController.submit_quiz)
+router.post('/1', quizController.submit_quiz)
 
 module.exports = router;
