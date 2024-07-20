c8085ec6e356ab97b158347df88c0b6f9695fd3f
Nokashi <44207343+Nokashi@users.noreply.github.com>
2024-07-20 18:50:14 +0300

    Add slideshow on main page

--- controllers/landingController.js
+++ controllers/landingController.js
@@ -24,11 +24,13 @@ async function fetchChapterQuizzesDone(userId, chapterNum) {
 }
 
 exports.landing_page = asyncHandler(async (req, res, next) => {
+    
     res.render('landing');
 })
 
 exports.landing_page_bill = asyncHandler(async (req, res, next) => {
-    res.render('nav_menu');
+    images = ['andalucia.webp', 'barcelona.webp', 'granada.jpg', 'madrid.jpg']
+    res.render('nav_menu', {images});
 })
 
 exports.courses_preview = asyncHandler(async (req, res, next) => {
---
+++ public/javascripts/slideshow.js
@@ -0,0 +1,32 @@
+let slideIndex = 0;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("slides");
+    if (n >= slides.length) { slideIndex = 0; }
+    if (n < 0) { slideIndex = slides.length - 1; }
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slides[slideIndex].style.display = "block";
+}
+
+// Auto-change slides every 3 seconds
+function autoSlides() {
+    let slides = document.getElementsByClassName("slides");
+    for (let i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    slideIndex++;
+    if (slideIndex > slides.length) { slideIndex = 1; }
+    slides[slideIndex - 1].style.display = "block";
+    setTimeout(autoSlides, 3000); // Change image every 3 seconds
+}
+
+// Initialize auto-slide
+autoSlides();
