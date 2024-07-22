537a9030f0a516de718e21d94cc01cf73bcd7fe1
Nokashi <44207343+Nokashi@users.noreply.github.com>
2024-07-22 14:49:17 +0300

    Add More Lesson Material

--- public/javascripts/chapter3_theory.js
+++ public/javascripts/chapter3_theory.js
@@ -1,10 +1,214 @@
 const chapter3_theory = [
-    "Spanish grammar revolves around gendered nouns (masculine and feminine), verb conjugations (present, past, future), and sentence structure (subject-verb-object). Learning basic grammar rules is essential for constructing meaningful sentences in Spanish.",
-    "Nouns in Spanish are categorized by gender (masculine or feminine) and number (singular or plural). Articles (definite and indefinite) precede nouns to indicate specificity and quantity. Understanding noun and article agreement is crucial for grammatical accuracy.",
-    "Verbs in Spanish are conjugated based on tense (present, past, future), mood (indicative, subjunctive), and person (first, second, third). Regular verbs follow predictable patterns, while irregular verbs have unique conjugations that must be memorized.",
-    "Pronouns (subject, object, reflexive) replace nouns to avoid repetition and clarify sentence structure. Prepositions establish relationships between words and phrases (location, direction, time). Mastering pronouns and prepositions enhances communication in Spanish.",
-    "Building vocabulary is fundamental for understanding and speaking Spanish fluently. Learn common phrases for everyday communication, including greetings, expressions of courtesy, and essential vocabulary related to family, food, travel, and leisure activities.",
-];
+    `<p>Τώρα που τελείωσες με τα λίγο πιο… τυπικά, ήρθε η ώρα να δούμε αυτό για το οποίο είσαι εδώ, την γλώσσα!
+    Θα ξεκινήσουμε με τα απλά πράγματα όπως τους χαιρετισμούς και πως να συστηθείς,
+    θα περάσουμε στην βασική κλίση ρημάτων, και θα δούμε και κάποιες βασικές λέξεις και φράσεις,
+    που θα σε βοηθήσουν να κινηθείς στην χώρα!</p>`,
+
+    `<p>Στον παρακάτω πίνακα, βλέπουμε τους βασικούς χαιρετισμούς που χρησιμοποιούν οι
+    ισπανοί στην καθημερινότητα τους και την μετάφρασή τους στα ελληνικά.
+    Το πρώτο πράγμα που θα παρατηρήσεις είναι ότι βάζουν κάποια
+    επιπλέον σημεία στίξης στην ερώτηση και τον θαυμασμό (στον γραπτό λόγο). </p>
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Ισπανικά</th>
+                    <th>Ελληνικά</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>¡Buenos días!</td>
+                    <td>«Καλημέρα!»</td>
+                </tr>
+                <tr>
+                    <td>¡Buenas tardes!</td>
+                    <td>«Καλό μεσημέρι!» <br>
+                    (Χρησιμοποιείται μετά τις 12 το μεσημέρι, <br> όπως εμείς χρησιμοποιούμε το καλησπέρα)</td>
+                </tr>
+                <tr>
+                    <td>¡Buenas noches!</td>
+                    <td>«Καλησπέρα» ή «Καληνύχτα» <br>
+                    (Χρησιμοποιείται και στις 2 περιπτώσεις)</td>
+                </tr>
+                <tr>
+                    <td>¡Hola!</td>
+                    <td>«Γεια σου/σας»</td>
+                </tr>
+                <tr>
+                    <td>¡Adiós!</td>
+                    <td>«Αντίο»</td>
+                </tr>
+                <tr>
+                    <td>¡Gracias! - ¡Muchas gracias!</td>
+                    <td>«Ευχαριστώ» - «Ευχαριστώ πολύ!»</td>
+                </tr>
+                <tr>
+                    <td>¡De nada!</td>
+                    <td>«Τίποτα!» </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>`,
+
+    `
+    <p>Τώρα πάμε να δούμε πως συστηνόμαστε και πως απαντάμε, με 3 διαφορετικούς τρόπους και την απάντηση τους.</p>
+
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Ισπανικά</th>
+                    <th></th>
+                    <th>Ελληνικά</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>¿Cómo te llamas?</td>
+                    <td>Me llamo Vasilis.</td>
+                    <td>-Πως σε λένε; <br> -Με λένε Βασίλη</td>
+                </tr>
+                <tr>
+                    <td>¿Cuál es tu nombre?</td>
+                    <td>Mi nombre es Vasilis.</td>
+                    <td>-Ποιο είναι το όνομα σου; <br> - Το όνομα μου είναι Βασίλης</td>
+                </tr>
+                <tr>
+                    <td>¿Quién eres tú?</td>
+                    <td>Yo soy Vasilis.</td>
+                    <td>-Ποιος είσαι εσυ; <br> -Εγώ είμαι ο Βασίλης</td>
+                </tr>
+                <tr>
+                    <td>¡Encantado de conocerte <br> Vasilis!</td>
+                    <td>(ή) Mucho gusto, Vasilis.</td>
+                    <td>Χαίρομαι που σε γνωρίζω <br> (2 διαφορετικοί τρόποι)</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <p>Τώρα, η πιο βασική ερώτηση που μπορεί να κάνει κάποιος, είναι να μάθει πως ονομάζεται κάτι στα Ισπανικά. Αυτό γίνεται με την εξής φράση.</p>
+    <p style="text-align: center;"> <b>¿Cómo se dice esto/eso en español? → Πως λέγεται αυτό/εκείνο στα ισπανικά;</b>
+    <p>Είναι από τις πιο χρήσιμες φράσεις που μπορεί να χρησιμοποιήσει κανείς για να μάθει νέες λέξεις.</p>
+    `,
+
+    `
+    <p>Ήδη μέσα από τις φράσεις αυτές, ξεχωρίζουν κάποια βασικά γραμματικά φαινόμενα,
+    όπως είναι οι αντωνυμίες και οι κλίσεις των ρημάτων. Οπότε ήρθε η ώρα να 
+    μάθουμε πως κλίνονται τα ρήματα στην ισπανική γλώσσα! Θα ξεκινήσουμε με το ρήμα <b>ser</b>
+    που σημαίνει «είμαι», όπου έχει και ανώμαλη κλίση. Μην ανησυχείς όμως,
+    διότι τα ομαλά ρήματα είναι πολύ πιο απλά.
+    Χωρίζονται σε 3 βασικές κλίσεις που ξεχωρίζουν από τις καταλήξεις του απαρέμφατου.</p>
+    
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Αντωνυμίες</th>
+                    <th>ser </th>
+                    <th>Ελληνικά</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Yo</td>
+                    <td>Soy</td>
+                    <td>Εγώ είμαι</td>
+                </tr>
+                <tr>
+                    <td>Tú</td>
+                    <td>Eres</td>
+                    <td>Εσύ είσαι</td>
+                </tr>
+                <tr>
+                    <td>él/ella </td>
+                    <td>Es</td>
+                    <td>Αυτός/αυτή είναι</td>
+                </tr>
+                <tr>
+                    <td>Nosotros</td>
+                    <td>Somos</td>
+                    <td>Εμείς είμαστε</td>
+                </tr>
+                <tr>
+                    <td>Vosotros</td>
+                    <td>Sois</td>
+                    <td>Εσείς είστε</td>
+                </tr>
+                <tr>
+                    <td>Ellos/Ellas</td>
+                    <td>Son</td>
+                    <td>Αυτοί/αυτές είναι</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+    <p>Οι κλίσεις που υπάρχουν είναι:</p>
+    <ul>
+        <li> Ρήματα σε <b>-ar</b> ( π.χ. hablar = μιλάω)
+        <li> Ρήματα σε <b>-er</b> (π.χ. comer = τρώω)
+        <li> Ρήματα σε <b>-ir</b> (π.χ. vivir = ζω)
+    </ul>
+    
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Κλίσεις ρημάτων</th>
+                    <th>-ar</th>
+                    <th>-er</th>
+                    <th>-ir</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Yo</td>
+                    <td>Habl-o</td>
+                    <td>Com-o</td>
+                    <td>Viv-o</td>
+                </tr>
+                <tr>
+                    <td>Tú</td>
+                    <td>Habl-as</td>
+                    <td>Com-es</td>
+                    <td>Viv-es</td>
+                </tr>
+                <tr>
+                    <td>él/ella</td>
+                    <td>Habl-a</td>
+                    <td>Com-e</td>
+                    <td>Viv-e</td>
+                </tr>
+                <tr>
+                    <td>Nosotros</td>
+                    <td>Habl-amos</td>
+                    <td>Com-emos</td>
+                    <td>Viv-imos</td>
+                </tr>
+                <tr>
+                    <td>Vosotros</td>
+                    <td>Habl-áis</td>
+                    <td>Com-éis</td>
+                    <td>Viv-ís</td>
+                </tr>
+                <tr>
+                    <td>Ellos/Ellas</td>
+                    <td>Habl-an</td>
+                    <td>Com-en</td>
+                    <td>Viv-en</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    
+    <p>Τώρα θα αναρωτιέσαι πως μπορείς να ξεχωρίζεις ποια κλίση να χρησιμοποιήσεις,
+    ωστόσο η απάντηση είναι απλή, κοιτάς την κατάληξη του απαρέμφατου και χρησιμοποιείς την ανάλογη κατάληξη.</p>`
+    ,
+
+
+    ];
 
 
 
@@ -19,7 +223,7 @@ function showQuizButton() {
         window.location.href = '/quizzes/3';
     };
     chapterContent.appendChild(quizButton);
-    quizButton.scrollIntoView({ behavior: 'smooth' });
+    // quizButton.scrollIntoView({ behavior: 'smooth' });
 }
 
 function showNextChapter() {
@@ -27,12 +231,13 @@ function showNextChapter() {
         const contentDiv = document.getElementById('content');
         const newDiv = document.createElement('div');
         newDiv.classList.add('chapter');
-        newDiv.textContent = chapter3_theory[currentChapter];
+        newDiv.innerHTML = chapter3_theory[currentChapter];
         contentDiv.appendChild(newDiv);
         newDiv.scrollIntoView({ behavior: 'smooth' });
         currentChapter++;
         if (currentChapter == chapter3_theory.length)
-        {
+        { 
+            document.getElementById('nextChapterButton').remove()
             showQuizButton();
         }
     } else {
