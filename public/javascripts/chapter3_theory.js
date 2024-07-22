f4c48ef9d0b1db82801edee8e39d960ecb110bfc
Nokashi <44207343+Nokashi@users.noreply.github.com>
2024-07-22 17:27:08 +0300

    Add more Chapter 3 Theory

--- public/javascripts/chapter3_theory.js
+++ public/javascripts/chapter3_theory.js
@@ -201,13 +201,282 @@ const chapter3_theory = [
                 </tr>
             </tbody>
         </table>
-    </div>
+    </div>`,
     
-    <p>Τώρα θα αναρωτιέσαι πως μπορείς να ξεχωρίζεις ποια κλίση να χρησιμοποιήσεις,
-    ωστόσο η απάντηση είναι απλή, κοιτάς την κατάληξη του απαρέμφατου και χρησιμοποιείς την ανάλογη κατάληξη.</p>`
-    ,
+    `<p>Μιας και πιάσαμε τα ρήματα, ας δούμε και κάποια ανώμαλα που χρησιμοποιούνται συχνά στην καθημερινότητα.
+    Μαζί με το <u>ser</u> υπάρχει και το <u>estar</u> που είναι ανώμαλο αλλά χρησιμοποιείται το ίδιο συχνά.</p>
+
+    <div class="table-container">
+        <table>
+            <tbody>
+                <tr>
+                    <td>Yo</td>
+                    <td>Estoy</td>
+                </tr>
+                <tr>
+                    <td>Tú</td>
+                    <td>Estas</td>
+                </tr>
+                <tr>
+                    <td>Él/Ella</td>
+                    <td>Está</td>
+                </tr>
+                <tr>
+                    <td>Nosotros</td>
+                    <td>Estamos</td>
+                </tr>
+                <tr>
+                    <td>Vosotros</td>
+                    <td>Estáis</td>
+                </tr>
+                <tr>
+                    <td>Ellos/Ellas</td>
+                    <td>Están</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <p>Στα ελληνικά, το ser και το estar έχουν την <b>ίδια</b> μετάφραση «είμαι».
+    Ωστόσο, το estar χρησιμοποιείται <u>όταν θέλουμε να εκφράσουμε κάτι <b>παροδικό</b></u> όπως για παράδειγμα η φράση:</p>
+
+    <p style="text-align: center;"> «<b>Estoy</b> en Barcelona por vacaciones» → «Είμαι στην Βαρκελώνη για διακοπες»</p>
+
+    <p>ενώ όπως είδαμε παραπάνω, το ser το χρησιμοποιούμε όταν θέλουμε να περιγράψουμε κάτι σταθερό,
+    όπως το όνομα μας, το επάγγελμα μας ή και κάποιο άλλο χαρακτηριστικό. </p>
+
+    <p style="text-align: center;"> «Me llamo Vasilis y soy delgado» → «Με λένε Βασίλη και είμαι ψηλός» </p>
+
+    `,
 
     `
+    <p>Το ύψος είναι κάτι που δεν αλλάζει στους ενήλικες, οπότε όταν θέλουμε να εκφράσουμε ότι κάποιος είναι ψηλός, χρησιμοποιούμε το <b>ser</b> αντί του estar.
+    Κάποια άλλα βασικά ρήματα που έχουν ανώμαλη κλίση είναι τα <b>tener</b>(έχω), <b>hacer</b> (κάνω), <b>ir</b> (πηγαίνω), <b>venir</b> (έρχομαι).
+    Χρησιμοποιούνται αρκετά συχνά όπως φαίνεται και από την μετάφραση τους, οπότε πάμε να δούμε πως κλίνονται.</p>
+
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Tener (έχω)</th>
+                    <th>Hacer (κάνω)</th>
+                    <th>Ir (πηγαίνω)</th>
+                    <th>Venir (έρχομαι)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Yo</td>
+                    <td>Tengo</td>
+                    <td>Hago</td>
+                    <td>Voy</td>
+                    <td>Vengo</td>
+                </tr>
+                <tr>
+                    <td>Tú</td>
+                    <td>Tienes</td>
+                    <td>Haces</td>
+                    <td>Vas</td>
+                    <td>Vienes</td>
+                </tr>
+                <tr>
+                    <td>Él/Ella</td>
+                    <td>Tiene</td>
+                    <td>Hace</td>
+                    <td>Va</td>
+                    <td>Viene</td>
+                </tr>
+                <tr>
+                    <td>Nosotros</td>
+                    <td>Tenemos</td>
+                    <td>Hacemos</td>
+                    <td>Vamos</td>
+                    <td>Venimos</td>
+                </tr>
+                <tr>
+                    <td>Vosotros</td>
+                    <td>Tenéis</td>
+                    <td>Hacéis</td>
+                    <td>Vais</td>
+                    <td>Venís</td>
+                </tr>
+                <tr>
+                    <td>Ellos/Ellas</td>
+                    <td>Tienen</td>
+                    <td>Hacen</td>
+                    <td>Van</td>
+                    <td>Vienen</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `,
+
+    `
+    <p>Τέλος, αξίζει να δούμε και τα ρήματα <b>decir</b> (λέω),
+    <b>saber</b> (ξέρω), <b>querer</b> (θέλω) και <b>preferir</b> (προτιμώ), καθώς είναι 
+    ρήματα που χρησιμοποιούνται εξίσου συχνά,
+    και θα τα δούμε και παρακάτω</p>
+
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Saber (ξέρω)</th>
+                    <th>Querer (θέλω)</th>
+                    <th>Decir (λέω)</th>
+                    <th>Preferir (προτιμώ)</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Yo</td>
+                    <td>Sé</td>
+                    <td>Quiero</td>
+                    <td>Digo</td>
+                    <td>Prefiero</td>
+                </tr>
+                <tr>
+                    <td>Tú</td>
+                    <td>Sabes</td>
+                    <td>Quieres</td>
+                    <td>Dices</td>
+                    <td>Prefieres</td>
+                </tr>
+                <tr>
+                    <td>Él/Ella</td>
+                    <td>Sabe</td>
+                    <td>Quiere</td>
+                    <td>Dice</td>
+                    <td>Prefiere</td>
+                </tr>
+                <tr>
+                    <td>Nosotros</td>
+                    <td>Sabemos</td>
+                    <td>Queremos</td>
+                    <td>Decimos</td>
+                    <td>Preferimos</td>
+                </tr>
+                <tr>
+                    <td>Vosotros</td>
+                    <td>Sabéis</td>
+                    <td>Queréis</td>
+                    <td>Decís</td>
+                    <td>Preferís</td>
+                </tr>
+                <tr>
+                    <td>Ellos/Ellas</td>
+                    <td>Saben</td>
+                    <td>Quieren</td>
+                    <td>Dicen</td>
+                    <td>Prefieren</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `,
+
+    `
+    <p>Αρκετά με τα ρήματα (για την ώρα), τώρα ας περάσουμε σε
+    κάποια άλλα σημαντικά πράγματα, όπως οι αριθμοί, πως
+    να λέμε την ώρα,
+    καθώς και τις ημέρες και τους μήνες. </p>
+
+    <div class="table-container">
+        <table>
+            <tbody>
+                <tr>
+                    <td>1: <b>uno</b> (ούνο)</td>
+                    <td>11: <b>once</b> (όνθε)</td>
+                    <td>21: <b>veintiuno</b> <br> (βέιντι-ούνο)</td>
+                </tr>
+                <tr>
+                    <td>2: <b>dos</b> (ντος)</td>
+                    <td>12: <b>doce</b> (ντόθε)</td>
+                    <td>22: <b>veintidós</b></td>
+                </tr>
+                <tr>
+                    <td>3: <b>tres</b> (τρες)</td>
+                    <td>13: <b>trece</b> (τρέθε)</td>
+                    <td>23: <b>veintitrés</b></td>
+                </tr>
+                <tr>
+                    <td>4: <b>cuarto</b> (κουάτρο)</td>
+                    <td>14: <b>catorce</b> (κατόρθε)</td>
+                    <td>24: <b>veinticuatro</b> </td>
+                </tr>
+                <tr>
+                    <td>5: <b>cinco</b> (θίνκο)</td>
+                    <td>15: <b>quince</b> (κίνθε)</td>
+                    <td>25: <b>veinticinco</b></td>
+                </tr>
+                <tr>
+                    <td>6: <b>seis</b> (σέις)</td>
+                    <td>16: <b>dieciséis</b> (ντιέθι-σέις)</td>
+                    <td>26: <b>veintiséis</b></td>
+                </tr>
+                <tr>
+                    <td>7: <b>siete</b> (σιέτε)</td>
+                    <td>17: <b>diecisiete</b> (ντιέθι-σιέτε)</td>
+                    <td>27: <b>veintisiete<b></td>
+                </tr>
+                <tr>
+                    <td>8: <b>ocho</b> (ότσο)</td>
+                    <td>18: <b>dieciocho</b> (ντιέθι-ότσο)</td>
+                    <td>28: <b>veintiocho</b>/td>
+                </tr>
+                <tr>
+                    <td>9: <b>nueve</b> (νουέβε)</td>
+                    <td>19: <b>diecinueve</b> (ντιέθι-νουέβε)</td>
+                    <td>29: <b>veintinueve</b></td>
+                </tr>
+                <tr>
+                    <td>10: <b>diez</b> (ντίεθ)</td>
+                    <td>20: <b>veinte</b> (βέιντε)</td>
+                    <td>30: <b>treinta</b> (τρέιντα)</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <p>Από το <b>30</b> και έπειτα, τα νούμερα είναι της παρακάτω μορφής:</p>
+
+    <p style="text-align: center;"><i>Treinta y </i>______ (uno, dos, ... nueve)</p>
+
+    <p>Και αυτό συμβαίνει για κάθε δεκάδα μέχρι το 100. 
+    Οπότε όταν θέλουμε να πούμε ένα νούμερο, χρησιμοποιούμε
+    το όνομα της αντίστοιχης δεκάδας συν το νούμερο από 1-9.</p>
+
+    <div class="table-container">
+        <table>
+            <tbody>
+                <tr>
+                    <td>40: <b>cuarenta</b> (κουαρέντα)</td>
+                    <td>80: <b>ochenta</b> (οτσέντα)</td>
+                </tr>
+                <tr>
+                    <td>50: <b>cincuenta</b> (θινκουέντα)</td>
+                    <td>90: <b>noventa</b> (νοβέντα)</td>
+                </tr>
+                <tr>
+                    <td>60: <b>sesenta</b> (σεσέντα)</td>
+                    <td>100: <b>cien</b> (θιέν)</td>
+                </tr>
+                <tr>
+                    <td>70: <b>setenta</b> (σετέντα)</td>
+                    <td>0: <b>cero</b> (θέρο)</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `,
+
+    `
+    <p>Ας περάσουμε σε κάτι εύκολο τώρα,
+    που είναι οι ημέρες της εβδομάδας και οι μήνες του χρόνου.</p>
+
     <div style="display: flex; gap: 5px;">
     <div class="table-container">
         <table>
@@ -293,6 +562,176 @@ const chapter3_theory = [
     </div>
     </div>
 
+    `,
+
+    `
+    <div style="display: flex; gap: 5px;">
+        <p style="justify-self: center; align-self: center;">Μιας και δεν έχουμε δει πολύ λεξιλόγιο ακόμα,
+        ας αναφέρουμε πως λέγονται οι λέξεις <b>ημέρα</b> και <b>μήνας</b>,
+        στον ενικό και τον πληθυντικό.</p>
+
+        <div class="table-container">
+            <table class="table">
+                <thead>
+                    <tr>
+                        <th></th>
+                        <th>Ημέρα</th>
+                        <th>Μήνας</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td><b>Ενικός</b></td>
+                        <td>El día</td>
+                        <td>El mes</td>
+                    </tr>
+                    <tr>
+                        <td><b>Πληθυντικός</b></td>
+                        <td>Los días</td>
+                        <td>Los meses</td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
+    </div
+    `,
+
+    `
+    <p>Τώρα, ήρθε η ώρα για κάτι που φαίνεται απλό αλλά
+    μπερδεύει την πρώτη φορά που το βλέπεις, την ώρα.
+    Οι ισπανοί χρησιμοποιούν παρόμοιο τρόπο με τα ελληνικά για να
+    την εκφράσουν. Έτσι λοιπόν, όταν θέλουν να πουν «Η ώρα είναι 1»
+    θα χρηστιμοποιήσουν ενικό, ενώ για τις υπόλοπες ώρες θα
+    χρησιμοποιήσουτν πληθυντικό όπως εμείς λέμε «Στις 2»
+    για παράδειγμα.
+    Ας δούμε ένα παράδειγμα:</p>
+
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Ισπανικά</th>
+                    <th>Ελληνικά</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>¿Qué hora es?»</td>
+                    <td>Τι ώρα είναι;</td>
+                </tr>
+                <tr>
+                    <td>«Es la una» </td>
+                    <td>Η ώρα είναι μία</td>
+                </tr>
+                <tr>
+                    <td>«Son las dos (2), ...doce(12)»</td>
+                    <td>Η ώρα είναι 2, ...12</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <p>Όταν θέλουμε να πούμε «και τέταρτο»,
+    «και μισή»,κλπ χρησιμοποιούμε τις παρακάτω φράσεις:</p>
+
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Ισπανικά</th>
+                    <th>Ελληνικά</th>
+                    <th>Ισπανικά</th>
+                    <th>Ελληνικά</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>En punto</td>
+                    <td>ακριβώς</td>
+                    <td>De la mañana</td>
+                    <td>το πρωί</td>
+                </tr>
+                <tr>
+                    <td>Y cuarto</td>
+                    <td>και τέταρτο</td>
+                    <td>De la tarde</td>
+                    <td>το μεσημέρι</td>
+                </tr>
+                <tr>
+                    <td>Y media</td>
+                    <td>και μισή</td>
+                    <td>De la noche</td>
+                    <td>το βραδυ</td>
+                </tr>
+                <tr>
+                    <td>Menos cuarto</td>
+                    <td>παρά τέταρτο</td>
+                    <td>y... menos...</td>
+                    <td>Και... Παρά...</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `,
+
+    `
+    <p>Τώρα είμαστε σε θέση να πούμε την ώρα
+    (μετά από λίγη εξάσκηση φυσικά), και μαζί με τις ημέρες και
+    τους μήνες, μπορούμε να ορίσουμε μια ημερομηνία. 
+    Υπάρχουν κάποιες φράσεις που μπορούμε να χρησιμοποιήσουμε 
+    επιπλέον, όπως το <b>σήμερα, αύριο, μεθαύριο</b>, κλπ.
+    Όλες αυτές οι λέξεις/φράσεις είναι συγκεντρωμένες παρακάτω.</p>
+
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Ελληνικά</th>
+                    <th>Ισπανικά</th>
+                    <th>Ελληνικά</th>
+                    <th>Ισπανικά</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Προχθές</td>
+                    <td>Anteayer</td>
+                    <td>Προηγουμενη εβδομαδα</td>
+                    <td>La semana pasada</td>
+                </tr>
+                <tr>
+                    <td>Εχθές</td>
+                    <td>Ayer</td>
+                    <td>Επόμενη εβδομάδα</td>
+                    <td>La próxima semana</td>
+                </tr>
+                <tr>
+                    <td>Σήμερα</td>
+                    <td>Hoy</td>
+                    <td>Προηγούμενος μηνας</td>
+                    <td>El mes pasado</td>
+                </tr>
+                <tr>
+                    <td>Αύριο</td>
+                    <td>Mañana</td>
+                    <td>Επόμενος μήνας</td>
+                    <td>El próximo mes</td>
+                </tr>
+                <tr>
+                    <td>Μεθαύριο</td>
+                    <td>Pasado mañana</td>
+                    <td>Προηγούμενο έτος</td>
+                    <td>El año pasado</td>
+                </tr>
+                <tr>
+                    <td>Σαββατοκύριακο</td>
+                    <td>El fin de semana</td>
+                    <td>Επόμενο έτος</td>
+                    <td>El próximo año</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
     `,
 
     ];
