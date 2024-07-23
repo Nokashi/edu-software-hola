873f688f84eadcb80f6e466a3071de2e67fc6f0b
Nokashi <44207343+Nokashi@users.noreply.github.com>
2024-07-23 15:34:35 +0300

    Start Ch4 Implementation

--- public/javascripts/chapter4_theory.js
+++ public/javascripts/chapter4_theory.js
@@ -1,9 +1,473 @@
 const chapter4_theory = [
-    "When dining in Spain, it's essential to know basic phrases for ordering food and drinks. Learn how to ask for a table ('¿Tiene una mesa disponible?'), request a menu ('La carta, por favor'), order food ('Quisiera...'), and ask about ingredients ('¿Qué lleva este plato?'). Familiarize yourself with common dishes and local specialties to enhance your culinary experience.",
-    "Navigating Spanish cities requires asking for directions ('¿Dónde está...?'). Learn how to inquire about the location of landmarks ('¿Dónde está la plaza mayor?'), find transportation hubs ('¿Dónde está la estación de tren?'), and get assistance with street names and addresses ('¿Cómo llego a...?'). Understanding directional phrases ('a la derecha', 'a la izquierda', 'recto') helps you navigate confidently.",
-    "Exploring Spanish markets and shops involves interacting in Spanish. Learn how to ask about prices ('¿Cuánto cuesta?'), request sizes ('¿Tiene una talla más grande?'), negotiate ('¿Me puede hacer un descuento?'), and express preferences ('Me gusta este color'). Practice essential shopping phrases to enjoy shopping for souvenirs and local products.",
-    "Planning activities in Spain often requires making reservations. Learn how to reserve a hotel room ('Quiero reservar una habitación'), book tickets ('Quisiera comprar entradas'), and secure spots for tours or events ('¿Puedo reservar para mañana?'). Understanding reservation etiquette and confirming details ensures smooth travel experiences.",
-    "In unforeseen circumstances, asking for help in Spanish is essential. Learn how to seek assistance in emergencies ('¡Ayuda! Necesito una ambulancia'), request information ('¿Puede ayudarme?'), and express urgency ('Es urgente'). Mastering basic phrases for seeking help ensures safety and support during your travels.",
+
+    `
+    <p>Μέχρι στιγμής, έχεις διαβάσει για βασικά γραμματικά φαινόμενα, κλίσεις ρημάτων, μπορείς να συστηθείς,
+    να αναφερθείς σε ώρα και ημερομηνία. Στην αρχή της προηγούμενης ενότητας, αναφέρθηκε η βασική φράση <b>«¿Cómo se dice?»</b> 
+    που μπορείς να χρησιμοποιήσεις για να μάθεις νέες λέξεις. Ας εμπλουτίσουμε
+    λοιπόν το λεξιλόγιο με κάποιες ακόμα φράσεις που μπορείς να χρησιμοποιήσεις αρκετά συχνά κατά την παραμονή σου στην χώρα.</p>
+    `,
+
+    `
+    <p>Οι «Ερωτηματικές λέξεις» είναι λέξεις που χρησιμοποιούνται για να λάβουμε
+    κάποια πληροφορία την οποία δεν γνωρίζουμε,
+    όπως για παράδειγμα να ζητήσουμε οδηγίες ή τον λογαριασμό.</p>
+
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Ισπανικα</th>
+                    <th>Ελληνικά</th>
+                    <th>Ισπανικά</th>
+                    <th>Ελληνικά</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Quién</td>
+                    <td>Ποιος</td>
+                    <td>¿Quién eres tú? </td>
+                    <td>Ποιος είσαι;</td>
+                </tr>
+                <tr>
+                    <td>Cómo</td>
+                    <td>Πως</td>
+                    <td>¿Cómo estás?</td>
+                    <td>Πώς είσαι;</td>
+                </tr>
+                <tr>
+                    <td>Cuándo</td>
+                    <td>Πότε</td>
+                    <td>¿Cuándo vas de vacaciones?</td>
+                    <td>Πότε πας διακοπές;</td>
+                </tr>
+                <tr>
+                    <td>Cuánto</td>
+                    <td>Πόσο</td>
+                    <td>¿Cuánto cuesta el café?</td>
+                    <td>Πόσο κάνει ο καφές;</td>
+                </tr>
+                <tr>
+                    <td>Dónde</td>
+                    <td>Πότε</td>
+                    <td>¿Dónde está el museo?</td>
+                    <td>Πού είναι το μουσείο;</td>
+                </tr>
+                <tr>
+                    <td>Qué</td>
+                    <td>Τι</td>
+                    <td>¿Qué hora es?</td>
+                    <td>Τι ώρα είναι;</td>
+                </tr>
+                <tr>
+                    <td>Cuál</td>
+                    <td>Ποιο</td>
+                    <td>¿Cuál es tu trabajo?</td>
+                    <td>Ποια είναι η δουλειά σου;</td>
+                </tr>
+                <tr>
+                    <td>¿Por qué?</td>
+                    <td>Γιατί;</td>
+                    <td>¿Por qué estudias español?</td>
+                    <td>Γιατί μελετάς ισπανικα;</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `,
+
+    `
+    <p>Ας κάνουμε τώρα μια μικρή επιστροφή στα ρήματα, καθώς είναι ώρα να δούμε αυτά που μπορείς να χρησιμοποιήσεις
+    για να εκφράσεις ενδιαφέρον. Αυτά που θα δούμε είναι τα <b>gustar</b> (θέλω), <b>encantar</b> (λατρεύω), <b>interesar</b> (ενδιαφέρω)
+    και το <b>haber</b> (έχω). Είναι λίγο διαφορετικά συγκριτικά με τα προηγούμενα που είδαμε καθώς το ρήμα δεν κλίνεται,
+    έχει ενικό και πληθυντικό όπου χρησιμοποιείται αναλόγως αν μιλάμε για ένα ή πολλά πράγματα.</p>
+
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th></th>
+                    <th>Αντωνυμία</th>
+                    <th>Ένα αντικείμενο</th>
+                    <th>Πολλά αντικείμενα</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td><b>Ενικός</b></td>
+                    <td>Me, Te, Le/La</td>
+                    <td>gusta, interesa, encanta</td>
+                    <td>gustan, interesan, encantan</td>
+                </tr>
+                <tr>
+                    <td><b>Πληθυντικός</b></td>
+                    <td>Nos, Os, Les</td>
+                    <td>gusta, interesa, encantan</td>
+                    <td>gustan, interesan, encantan</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+
+    <p>Και για να είναι πιο εύκολο στην κατανόηση, ας δούμε κάποια παραδείγματα.</p
+    `,
+
+    `
+    <p>Η φράση <b>«Me encanta el mar»</b> σημαίνει «<b>Λατρεύω</b> την θάλασσα»,
+    ενώ η φράση <b>«Me gustan los bailes tradicionales»</b> μεταφράζεται ως «Μου <b>αρέσουν</b> οι παραδοσιακοί χοροί».
+    Όπως βλέπεις,
+    χρησιμοποίησαμε το <u>gustan</u> όταν θέλαμε να αναφερθούμε στους παραδοσιακούς χορούς, ενώ το <u>encanta</u> όταν αναφερθήκαμε στην θάλασσα.</p> <br><br>
+
+    <p>Αναφορικά με το <b>haber</b>, αυτό που χρειάζεται να θυμάσαι είναι το <b>hay</b> που μεταφράζεται ως «υπάρχει».
+    Οπότε αν θέλουμε να πούμε «Υπάρχει μια πολή όμορφη παραλία στην Βαρκελώνη»,
+    μπορούμε να το πούμε ως εξής:  <b>«Hay una playa muy bonita en Barcelona»</b>.</p> <br><br>
+    `,
+
+    `
+    <p>Το ακριβώς παραπάνω παράδειγμα περιέχει το επίθετο <b>bonito</b> (bonita όταν είναι θυληκό το ουσιαστικό που ακολουθεί)
+    που σημαίνει όμορφο. Ευκαιρία να ασχοληθούμε με λίγο καθαρό λεξιλόγιο,
+    και να δούμε κάποια βασικά επίθετα που χρησιμοποιούνται όταν θέλουμε να περιγράψουμε πράγματα και καταστάσεις στα ισπανικά.</p>
+
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Ελληνικα</th>
+                    <th>Ισπανικά</th>
+                    <th>Ελληνικά</th>
+                    <th>Ισπανικά</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Μικρό</td>
+                    <td>Pequeño/a</td>
+                    <td>Νέο</td>
+                    <td>Nuevo/a</td>
+                </tr>
+                <tr>
+                    <td>Μεγάλο</td>
+                    <td>Grande</td>
+                    <td>Παλιό</td>
+                    <td>Viejo/a</td>
+                </tr>
+                <tr>
+                    <td>Καλό</td>
+                    <td>Bueno/a</td>
+                    <td>Άνετο</td>
+                    <td>Cómodo</td>
+                </tr>
+                <tr>
+                    <td>Κακό</td>
+                    <td>Malo/a</td>
+                    <td>Άβολο</td>
+                    <td>Incomodo</td>
+                </tr>
+                <tr>
+                    <td>Καθαρό</td>
+                    <td>Limpio</td>
+                    <td>Ενδιαφέρον</td>
+                    <td>Interesante</td>
+                </tr>
+                <tr>
+                    <td>Βρώμικο</td>
+                    <td>Sucio</td>
+                    <td>Βαρετό</td>
+                    <td>Aburrido/a</td>
+                </tr>
+                <tr>
+                    <td>Όμορφο</td>
+                    <td>Bonito/a – guapo/a</td>
+                    <td>Πολύ καλό</td>
+                    <td>Muy bueno/a – Bomba</td>
+                </tr>
+                <tr>
+                    <td>Άσχημο</td>
+                    <td>Feo/a</td>
+                    <td>Πολύ κακό</td>
+                    <td>Muy malo/a – fatal</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `,
+
+    `
+    <p>Τα επίθετα συνήθως πηγαίνουν <b>μετά</b> το ουσιαστικό,με
+    κάποιες εξαιρέσεις. Επίσης, εδώ μας απασχολεί για
+    πρώτη φορά το ζήτημα του άρθρου. Όπως και στα ελληνικά, 
+    έτσι και στα ισπανικά οι λέξεις χωρίζονται σε αρσενικο –
+    θυληκό – ουδέτερο, και τα επίθετα θα πάρουν την αντίστοιχη
+    κατάληξη του ουσιαστικού που το συνοδεύει.
+    Ας δούμε κάποια παραδείγματα για να γίνει πιο κατανοητό.</p>
+
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Ισπανικά</th>
+                    <th>Ελληνικά</th>
+                    <th>Ισπανικά</th>
+                    <th>Ελληνικά</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>La habitación pequeña</td>
+                    <td>Το μικρό δωμάτιο</td>
+                    <td>El nuevo móvil</td>
+                    <td>Το νέο κινητό</td>
+                </tr>
+                <tr>
+                    <td>El coche es grande</td>
+                    <td>Το αμάξι είναι μεγάλο</td>
+                    <td>La iglesia vieja</td>
+                    <td>Η αρχαία εκκλησία</td>
+                </tr>
+                <tr>
+                    <td>Un buen amigo</td>
+                    <td>Ένας καλός φίλος</td>
+                    <td>La cama cómoda</td>
+                    <td>Το βολικό κρεβάτι</td>
+                </tr>
+                <tr>
+                    <td>El tiempo está malo</td>
+                    <td>Ο καιρός είναι άσχημος</td>
+                    <td>El sofá incómodo</td>
+                    <td>Ο άβολος καναπές</td>
+                </tr>
+                <tr>
+                    <td>La mesa está limpia</td>
+                    <td>Το τραπέζι είναι καθαρό</td>
+                    <td>La película interesante</td>
+                    <td>Η ενδιαφέρουσα ταινία</td>
+                </tr>
+                <tr>
+                    <td>La playa está sucia</td>
+                    <td>Η παραλία είναι βρώμικη</td>
+                    <td>La presentación fue aburrida</td>
+                    <td>Η παρουσίαση ήταν βαρετή.</td>
+                </tr>
+                <tr>
+                    <td>Esa casa es bonita</td>
+                    <td>Εκείνο το σπίτι είναι όμορφο</td>
+                    <td>¡Lo pasamos bomba!</td>
+                    <td>Τα περάσαμε τέλεια!</td>
+                </tr>
+                <tr>
+                    <td>Este plato parece feo</td>
+                    <td>Αυτό το πιάτο φαίνεται άσχημο</td>
+                    <td>El servicio fue fata</td>
+                    <td>Η εξυπηρέτηση ήταν άθλια.</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `,
+
+    `
+    <p>Μήπως πείνασες;</p> <br><br>
+
+    <p>Όπως είδαμε στην δεύτερη ενότητα, η Ισπανία έχει πολλά
+    διάσημα και γευστικά πιάτα να δοκιμάσεις. Είτε πας σε
+    παραδοσιακή <b>taberna</b> (ναι, είναι το ίδιο με τα ελληνικά),
+    είτε σε κάποιο <b>restaurante moderno</b> (μοντέρνο εστιατόριο),
+    θα σου φανεί χρήσιμο να γνωρίζεις τις βασικές λέξεις και
+    φράσεις που αφορούν το φαγητό και το ποτό. Με αυτό τον 
+    τρόπο θα είσαι σε θέση να κατανοήσεις καλύτερα τον κατάλογο,
+    και τις γευστικές παραδόσεις που προσφέρει η χώρα.</p>
+
+    <p style="text-align: center;"> <u>Βασικές Λέξεις</u></p>
+
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Ισπανικά</th>
+                    <th>Ελληνικά</th>
+                    <th>Ισπανικά</th>
+                    <th>Ελληνικά</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>La comida (κόμιδα)</td>
+                    <td>φαγητό</td>
+                    <td>El vino blanco (βίνο μπλάνκο)</td>
+                    <td>Λευκό κρασί</td>
+                </tr>
+                <tr>
+                    <td>La bebida (μπεμπίδα)</td>
+                    <td>ποτό</td>
+                    <td>El agua (άγουα)</td>
+                    <td>νερό</td>
+                </tr>
+                <tr>
+                    <td>El desayuno (ντεσαγιούνο)</td>
+                    <td>πρωινό</td>
+                    <td>La Cerveza (θερβέθα)</td>
+                    <td>μπύρα</td>
+                </tr>
+                <tr>
+                    <td>El almuerzo (αλμουέρθο) </td>
+                    <td>μεσημεριανό</td>
+                    <td>El zumo (θούμο)</td>
+                    <td>χυμός</td>
+                </tr>
+                <tr>
+                    <td>La cena (θένα) </td>
+                    <td>βραδινό</td>
+                    <td>El café (καφέ) </td>
+                    <td>καφές</td>
+                </tr>
+                <tr>
+                    <td>El aperitivo (απεριτίβο)</td>
+                    <td>ορεκτικό</td>
+                    <td>El té (τε)</td>
+                    <td>τσάι</td>
+                </tr>
+                <tr>
+                    <td>El postre (πόστρε)</td>
+                    <td>γλυκό</td>
+                    <td>La ensalada (ενσαλάδα)</td>
+                    <td>σαλάτα</td>
+                </tr>
+                <tr>
+                    <td>El vino tinto (βίνο τίντο)</td>
+                    <td>κόκκινο κρασί</td>
+                    <td>La botella (μποτέγια) </td>
+                    <td>μπουκάλι</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `,
+
+    `
+    <p>Ας δούμε τώρα ποιες φράσεις μπορείς να χρησιμοποιήσεις
+    για να παραγγείλεις φαγητό, ποτό, να ζητήσεις λογαριασμό, κλπ.</p>
+
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Ισπανικά</th>
+                    <th>Ελληνικά</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Una mesa para ... personas</td>
+                    <td>Ένα τραπέζι για ... άτομα</td>
+                </tr>
+                <tr>
+                    <td>Quisiera... (Κισιέρα)</td>
+                    <td>Θα ήθελα... </td>
+                </tr>
+                <tr>
+                    <td>¿Qué recomienda?</td>
+                    <td>Τι προτείνετε;</td>
+                </tr>
+                <tr>
+                    <td>¡La cuenta, por favor! (λα κουέντα πορ φαβόρ)</td>
+                    <td>Τον λογαριασμό, παρακαλώ!</td>
+                </tr>
+                <tr>
+                    <td>Con ... / Sin ... </td>
+                    <td>Με ... / Χωρίς ... </td>
+                </tr>
+                <tr>
+                    <td>¿Dónde está el aseo, por favor?</td>
+                    <td>Πού είναι η τουαλέτα, παρακαλω;</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `,
+
+    `
+    <p>Τώρα ας δούμε κάποιες ακόμα βασικές λέξεις
+    που αφορούν τα πιάτα και συστατικά που μπορεί να συναντήσεις.</p>
+
+    <div class="table-container">
+        <table>
+            <thead>
+                <tr>
+                    <th>Ισπανικά</th>
+                    <th>Ελληνικά</th>
+                    <th>Ισπανικά</th>
+                    <th>Ελληνικά</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>Una paella </td>
+                    <td>Παέγια</td>
+                    <td>Los mariscos (μαρίσκος)</td>
+                    <td>θαλασσινά</td>
+                </tr>
+                <tr>
+                    <td>Las tapas </td>
+                    <td>παραδοσιακά ορεκτικά</td>
+                    <td>Las verduras (βερδούρας)</td>
+                    <td>λαχανικά</td>
+                </tr>
+                <tr>
+                    <td>Una tortilla de patatas</td>
+                    <td>ομελέτα με πατάτα</td>
+                    <td>Las frutas</td>
+                    <td>φρούτα</td>
+                </tr>
+                <tr>
+                    <td>El gazpacho (γκαθπάτσο)</td>
+                    <td>Κρύα σούπα/td>
+                    <td>El queso (κέσο)</td>
+                    <td>τυρί</td>
+                </tr>
+                <tr>
+                    <td>El jamón (χαμόν) </td>
+                    <td>ίδος αλλαντικού</td>
+                    <td>El Aceite (ασέιτε)</td>
+                    <td>ελαιόλαδο</td>
+                </tr>
+                <tr>
+                    <td>El pollo (πόγιο) </td>
+                    <td>κοτόπουλο</td>
+                    <td>El ajo (άχο) </td>
+                    <td>σκόρδο</td>
+                </tr>
+                <tr>
+                    <td>La carne (κάρνε)</td>
+                    <td>κρέας</td>
+                    <td>La cebolla (σεμπόγια)</td>
+                    <td>κρεμμύδι</td>
+                </tr>
+                <tr>
+                    <td>El pescado (πεσκάδο) </td>
+                    <td>ψάρι</td>
+                    <td>El tomate</td>
+                    <td>ντομάτα</td>
+                </tr>
+                <tr>
+                    <td>El pimiento (πιμιέντο)</td>
+                    <td>πιπεριά</td>
+                    <td>El pepino (πεπίνο) </td>
+                    <td>αγγούρι</td>
+                </tr>
+                <tr>
+                    <td>La gamba (γκάμπα)</td>
+                    <td>γαρίδα</td>
+                    <td>El atún (ατούν) </td>
+                    <td>τόνος</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `,
+
+
+
+    
 ];
 
 let currentChapter = 0;
