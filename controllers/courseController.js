14e8381a18cd0bdee8f74b90dfa7c03fd5eaea76
Nokashi <44207343+Nokashi@users.noreply.github.com>
2024-07-23 22:17:15 +0300

    Add Course interactivity

--- controllers/courseController.js
+++ controllers/courseController.js
@@ -1,34 +1,34 @@
 const asyncHandler = require('express-async-handler');
+const User = require('../models/user');
 
 
 exports.course_list = asyncHandler(async (req, res, next) => {
     if(req.isAuthenticated())
     {
-        res.render('courseList');
+        const userId = req.user._id
+        const userData = await User.findById(userId);
+        const hasCompleted_ch3 = userData.performance_history.some(history => history.quiz_chapter === 3);
+        res.render('courseList', {hasCompleted_ch3});
     } 
     else {
-        res.render('courses-preview')
+        res.render('session-expired')
     }
 })
 
 exports.course1 = asyncHandler(async (req, res, next) => {
     res.render('course1');
-
 })
 
 exports.course2 = asyncHandler(async (req, res, next) => {
     res.render('course2');
-
 })
 
 exports.course3 = asyncHandler(async (req, res, next) => {
     res.render('course3');
-
 })
 
 exports.course4 = asyncHandler(async (req, res, next) => {
     res.render('course4');
-
 })
 
 
