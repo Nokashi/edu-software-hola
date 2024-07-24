1430d85b3c55a489d29c402b737a25d0f087a3b2
Nokashi <44207343+Nokashi@users.noreply.github.com>
2024-07-24 12:59:14 +0300

    Update Retention Button

--- controllers/landingController.js
+++ controllers/landingController.js
@@ -10,8 +10,12 @@ exports.landing_page = asyncHandler(async (req, res, next) => {
 exports.landing_page_bill = asyncHandler(async (req, res, next) => {
     if(req.isAuthenticated())
     {
-        images = ['escorial.jpg', 'prado.jpg', 'guell.jpg', 'segrada.jpg']
-        res.render('nav_menu', {images});
+        const userId = req.user._id
+        const userData = await User.findById(userId).select('performance_history').lean();
+
+        // Check if performance_history array is not empty
+        const hasCompletedQuiz = userData && userData.performance_history && userData.performance_history.length > 1;
+        res.render('nav_menu', {hasCompletedQuiz});
     } else 
     {
         images = ['escorial.jpg', 'prado.jpg', 'guell.jpg', 'segrada.jpg']
